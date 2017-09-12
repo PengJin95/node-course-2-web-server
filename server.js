06a4d38ec7d0d3239e0cca5004f69782d5ae4ba6
pengjin95 <jinpeng_abroad@163.com>
2017-09-12 12:54:30 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use(function(req, res, next) {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next();
+});
+
+// app.use(function(req, res, next) {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', function() {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text) {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome!',
+        currentYear: new Date().getFullYear()
+    })
+});
+
+app.get('/about', function(req, res) {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', function(req, res) {
+    res.send({
+        errorMessage: 'bad request'
+    });
+});
+
+
+app.listen(3000, function() {
+    console.log('loading');
+});
